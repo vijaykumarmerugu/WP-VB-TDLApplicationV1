61ad93496c7d231bce56913b53e159dbda190298
vijay kumar <vijay@vijay.com>
2023-12-04 11:09:55 -0600

    Initial Commit

---
+++ src/main/java/com/WPVBTDLApplicationV1/WPVBTDLApplicationV1/WpVbTdlApplicationV1Application.java
@@ -0,0 +1,13 @@
+package com.WPVBTDLApplicationV1.WPVBTDLApplicationV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpVbTdlApplicationV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpVbTdlApplicationV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/WPVBTDLApplicationV1/WPVBTDLApplicationV1/WpVbTdlApplicationV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WPVBTDLApplicationV1.WPVBTDLApplicationV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpVbTdlApplicationV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
